d96e455f0baaceefaf84d7c67d6f9ad20d3da163
Tabin_Jasper_BSIT32E1 <tabinjasper@gmail.com>
2024-04-12 23:41:14 +0800

    Added a Model to manage user data for login and registration functionality.

---
+++ FULLSTACK_MTA1_LoginRegister/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FULLSTACK_MTA1_LoginRegister.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
