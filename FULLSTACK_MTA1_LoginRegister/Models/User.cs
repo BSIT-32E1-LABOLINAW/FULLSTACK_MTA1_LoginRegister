acccf7e402cddac11b854a11621f68960611336e
Tabin_Jasper_BSIT32E1 <tabinjasper@gmail.com>
2024-04-12 23:35:48 +0800

    Added a Model for User includes Username & Password

---
+++ FULLSTACK_MTA1_LoginRegister/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace FULLSTACK_MTA1_LoginRegister.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
