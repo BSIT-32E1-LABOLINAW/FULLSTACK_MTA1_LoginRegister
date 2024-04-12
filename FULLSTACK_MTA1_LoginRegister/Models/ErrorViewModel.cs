318964bf59b8c95f2f0da631b90c45f66af3eb69
Tabin_Jasper_BSIT32E1 <tabinjasper@gmail.com>
2024-04-12 22:39:46 +0800

    Add project files.

---
+++ FULLSTACK_MTA1_LoginRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FULLSTACK_MTA1_LoginRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
