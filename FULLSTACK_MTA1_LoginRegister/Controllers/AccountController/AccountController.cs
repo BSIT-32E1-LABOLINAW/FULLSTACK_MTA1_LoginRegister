abfb737d5889ed93bc70200da2f5598fa9297bb5
De Luna, Mart Dexter <153422801+DeLuna-MartDexter-BSIT32E1@users.noreply.github.com>
2024-04-13 01:35:28 +0800

    Not Working, but it has a controller for attempts and login

---
+++ FULLSTACK_MTA1_LoginRegister/Controllers/AccountController/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FULLSTACK_MTA1_LoginRegister.Controllers.AccountController
+{
+    using FULLSTACK_MTA1_LoginRegister.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+    using System.Security.Cryptography;
+    using System.Text.RegularExpressions;
+
+
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+        private const int LoginAttempts = 3;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // LOGIN SECTION
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var loggedInUser = _context.Users.FirstOrDefault(u => u.Username == user.Username);
+
+            if (loggedInUser != null && VerifyPassword(user.Password, loggedInUser.Password))
+            {
+                HandleSuccessfulLogin(loggedInUser.Username);
+                return RedirectToAction("Index", "Home");
+            }
+
+            HandleFailedLogin();
+            return View(user);
+        }
+
+        private void HandleSuccessfulLogin(string username)
+        {
+            HttpContext.Session.SetString("Username", username);
+            HttpContext.Session.SetInt32("LoginAttempts", 0);
+        }
+
+        private void HandleFailedLogin()
+        {
+            var remainingAttempts = LoginAttempts - IncrementLoginAttempt();
+            if (remainingAttempts > 0)
+                ModelState.AddModelError("", $"Invalid username or password. {remainingAttempts} attempts remaining.");
+            else
+                ModelState.AddModelError("", $"You have exceeded the maximum number of login attempts. Please try again later or re-register.");
+        }
+    }
